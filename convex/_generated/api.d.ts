51db1b1d30e59aa9beb99086819612a010bd4922
Arnold Gamboa <arnold.gamboa@gmail.com>
2025-11-29 13:38:21 +0800

    feat: add blogs module to API utility for improved functionality

--- convex/_generated/api.d.ts
+++ convex/_generated/api.d.ts
@@ -8,6 +8,7 @@
  * @module
  */
 
+import type * as blogs from "../blogs.js";
 import type * as categories from "../categories.js";
 import type * as directory from "../directory.js";
 import type * as http from "../http.js";
@@ -24,6 +25,7 @@ import type {
 } from "convex/server";
 
 declare const fullApi: ApiFromModules<{
+  blogs: typeof blogs;
   categories: typeof categories;
   directory: typeof directory;
   http: typeof http;
